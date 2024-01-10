0b3b296f74011321ec90a1b2f072c48b5dfc6000
Daniel Honório Júnior <154471089+enghonorios@users.noreply.github.com>
2024-01-10 19:33:49 -0300

    FUNÇÕES_JAVASCRIPT

---
+++ funções.js
@@ -0,0 +1,40 @@
+/** function teste (params) {
+    bloco de codigos console.log('teste');
+}
+teste(); -> executor da função
+*/
+
+/**  1 programa  function myName(name) {
+         console.log('Your name is:' + name);
+}
+
+
+myName('Daniel');
+myName('Debora');
+
+2 programa  function quadrado(valor) {
+    return valor * valor;
+}
+
+const quadradoDeDez = quadrado(10);
+console.log(quadradoDeDez); 
+
+function incrementarJuros(valor, percentualJuros) {
+    const valorDeAcrescimo = (percentualJuros / 100) * valor;
+    return valor + valorDeAcrescimo;
+}
+
+console.log(incrementarJuros(100, 10))
+console.log(incrementarJuros(100, 15))
+ */
+
+function calcularJuros() {
+
+}
+
+function main() {
+    console.log('Programa principal');
+    calcularJuros();
+}
+
+main();
